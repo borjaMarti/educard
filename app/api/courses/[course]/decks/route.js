4c3e51f1254369856c815f8d12901b1494e33d5f
Borja Martí <borjamarti@outlook.com>
2023-06-17 12:22:04 +0200

    api decks route done

--- app/api/courses/[course]/decks/route.js
+++ app/api/courses/[course]/decks/route.js
@@ -4,13 +4,14 @@ import Deck from '@/models/Deck';
 import Course from '@/models/Course';
 import { auth } from '@clerk/nextjs';
 
-export async function POST(req) {
+export async function POST(req, { params }) {
   await dbConnect();
   const { userId } = auth();
   const data = await req.json();
 
   try {
-    const { deckName, courseId } = data;
+    const courseId = params.course;
+    const { deckName } = data;
     const course = await Course.findOne({ _id: courseId, ownerId: userId });
 
     if (!course) {
@@ -33,45 +34,15 @@ export async function POST(req) {
   }
 }
 
-export async function GET(req) {
+export async function GET(req, { params }) {
   await dbConnect();
-  const data = await req.json();
 
   try {
-    const { courseId } = data;
+    const courseId = params.course;
     const decks = await Deck.find({ courseId: courseId }).select('_id deckName');
 
     return NextResponse.json(decks);
   } catch(err) {
     console.log(err);
   }
-}
-
-export async function PUT(req) {
-  await dbConnect();
-  const { userId } = auth();
-  const data = await req.json();
-
-  try {
-    const { deckId, content } = data;
-    const course = await Course.findOne({ deckIds: deckId, ownerId: userId });
-
-    if (!course) {
-      return NextResponse.json({ error: 'Unauthorized access' });
-    }
-
-    const updatedCourse = await Deck.findOneAndUpdate(
-          { _id: courseId },
-          { courseName: content },
-          { new: true }
-        );
-
-    return NextResponse.json(updatedCourse);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-export async function DELETE(req) {
-
 }
