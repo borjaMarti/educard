5633b4a397e6f3f1bd874fb6dca46ae742db6a69
Borja Martí <borjamarti@outlook.com>
2023-06-22 01:50:22 +0200

    test homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,17 @@
-const HomePage = () => {
+const HomePage = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/courses");
+    const data = await res.json();
+    console.log(data);
+  } catch(err) {
+    console.log(err);
+  }
 
-}
+  return (
+    <>
+      <h1>TEST</h1>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
