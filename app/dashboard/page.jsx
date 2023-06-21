33a42d0485b59467538a7356e341fe8b9ab967a7
Borja Martí <borjamarti@outlook.com>
2023-06-22 01:52:32 +0200

    intended dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,20 +1,37 @@
-import { currentUser } from '@clerk/nextjs'
+async function fetchCourses() {
+  const response = await fetch("http://localhost:3000/api/courses/6492b31fd1ee33e8e884ddab/decks");
+  const courses = await response.json();
+  return courses;
+}
 
 const DashboardPage = async () => {
-  const user = await currentUser();
+  const courses = await fetchCourses();
+  const { ownedCourses, studentCourses } = courses;
 
   return (
     <>
       <h2>Study</h2>
         <ul>
-
+          {studentCourses.map((course) => (
+            <li key={course._id}>
+              <Link href={`dashboard/courses/${course._id}`}>
+                <h3>{course.courseName}</h3>
+              </Link>
+            </li>
+          ))}
         </ul>
       <h2>Your Classes</h2>
         <ul>
-
+          {ownedCourses.map((course) => (
+            <li key={course._id}>
+              <Link href={`dashboard/courses/${course._id}`}>
+                <h3>{course.courseName}</h3>
+              </Link>
+            </li>
+          ))}
         </ul>
     </>
-  )
+  );
 }
 
-export default DashboardPage
+export default DashboardPage;
