7c51131a539742a674654a822faf11083b50ed24
Borja Martí <borjamarti@outlook.com>
2023-06-30 01:54:08 +0200

    created study component for main educard functionality, card studying

---
+++ app/components/StudyApp.jsx
@@ -0,0 +1,83 @@
+'use client'
+import { useState } from 'react';
+import { useParams, useRouter } from 'next/navigation';
+
+const StudyApp = ({ cards, focus }) => {
+  const params = useParams();
+
+  const [studyCards, setStudyCards] = useState(cards);
+  const [currentCard, setCurrentCard] = useState(studyCards[studyCards.length - 1]);
+  const [front, setFront] = useState(true);
+  const router = useRouter();
+
+  const totalCards = cards.length;
+
+  const handleToggleFront = () => {
+    setFront(!front);
+  }
+
+  const handleRepeat = async (id) => {
+    if (focus) {
+      await fetch(`/api/courses/${params.course}/decks/${params.deck}/cards/${id}/reminder`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({result: 0})
+      });
+    }
+
+    let newCards = [...studyCards];
+    newCards.unshift(newCards.pop());
+
+    setStudyCards(newCards);
+    setCurrentCard(newCards[newCards.length - 1]);
+    setFront(!front);
+  }
+
+  const handleGood = async (id) => {
+    if (focus) {
+      await fetch(`/api/courses/${params.course}/decks/${params.deck}/cards/${id}/reminder`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({result: 1})
+      });
+    }
+
+    if (studyCards.length === 1) {
+      console.log(currentCard)
+      return router.push(`/dashboard/courses/${params.course}`);
+    }
+
+    let newCards = [...studyCards];
+    newCards.pop();
+
+    setStudyCards(newCards);
+    setCurrentCard(newCards[newCards.length - 1]);
+    setFront(!front);
+  }
+
+  return (
+    <>
+      <span>{totalCards - studyCards.length + 1} / {totalCards}</span>
+      {front &&
+        <>
+          <h2>{currentCard.front}</h2>
+          <button onClick={handleToggleFront}>Mostrar Respuesta</button>
+        </>
+      }
+
+      {!front &&
+        <>
+          <h2>{currentCard.back}</h2>
+          <button onClick={() => handleRepeat(currentCard._id)}>Repetir</button>
+          <button onClick={() => handleGood(currentCard._id)}>Bien</button>
+        </>
+      }
+    </>
+  );
+};
+
+export default StudyApp;
