3c571c7772c8c3b88d30377e4b136538d88508c1
Borja Martí <borjamarti@outlook.com>
2023-07-15 18:20:11 +0200

    removed test code

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,4 @@
-import { auth } from "@clerk/nextjs";
-
 const HomePage = async () => {
-  const authResponse = auth();
-  const bearerToken = await authResponse.getToken({});
-
-  try {
-    const res = await fetch("http://localhost:3000/api/courses", { headers: { 'Authorization': `Bearer ${bearerToken}`}});
-    const data = await res.json();
-    console.log(data);
-  } catch(err) {
-    console.log(err);
-  }
 
   return (
     <>
