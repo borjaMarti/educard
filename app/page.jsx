a127cfbfcce7c79255e0ed8294ba035d805d3230
Borja Martí <borjamarti@outlook.com>
2023-08-28 00:14:12 +0200

    added hero image, started layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+
 const HomePage = async () => {
   return (
-    <>
-      <h1>Welcome to Educard!</h1>
-    </>
+    <main className="home-main">
+      <section className="hero">
+        <Image
+          src="/hero.svg"
+          alt="Chica estudiando."
+          height={0}
+          width={0}
+          style={{ width: "100%", height: "auto", maxWidth: "30rem" }}
+        />
+        <h1>Welcome to Educard!</h1>
+      </section>
+    </main>
   );
 };
 
