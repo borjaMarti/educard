63b845925cf058239707827dfbfa0eb094993f36
Borja Martí <borjamarti@outlook.com>
2023-09-14 15:51:39 +0200

    try to fix metadata bug

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,6 +7,7 @@ import Header from "@/components/ui/header-comp";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+  metadataBase: "https://educard.es",
   title: "EduCard",
   description: "Potencia el aprendizaje en el aula y más allá.",
   keywords: ["Estudiar", "Educación", "Flashcard"],
