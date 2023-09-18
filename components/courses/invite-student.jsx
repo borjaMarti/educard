928deaca4d21a7ef7565a9d3183566ff1b89e19a
Borja Martí <borjamarti@outlook.com>
2023-09-18 14:20:04 +0200

    fixed focus implementation on invite-student component

--- components/courses/invite-student.jsx
+++ components/courses/invite-student.jsx
@@ -75,8 +75,7 @@ const InviteStudent = () => {
             placeholder="Escribe aquí"
             onChange={(e) => setText(e.target.value)}
             disabled={isSubmitted}
-            className="dialog__input"
-            autoFocus
+            className="dialog__input focus"
           />
         </form>
         <div className="dialog__controls">
