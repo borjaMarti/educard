c9b750c092f3e96503ba0494df016acf398345aa
Borja Martí <borjamarti@outlook.com>
2023-09-22 11:23:43 +0200

    fixed footer nav ul children to be li themselves and not div, which is problematic for screen readers

--- components/ui/footer-comp.jsx
+++ components/ui/footer-comp.jsx
@@ -7,39 +7,39 @@ const Footer = () => {
       <div className="site-footer__separator"></div>
       <nav className="site-footer__nav">
         <ul className="site-footer__list">
-          <div>
-            <li>
-              <Link href="/tos" className="site-footer__link">
-                Condiciones
-              </Link>
-            </li>
-            <li>
-              <Link href="/privacy" className="site-footer__link">
-                Privacidad
-              </Link>
-            </li>
-          </div>
-          <div>
-            <li className="site-footer__madeby">
-              hecho con ❤️ por{" "}
-              <a
-                href="https://borjamarti.netlify.app/"
-                className="link link--footer"
-              >
-                borjaMartí
-              </a>
-            </li>
-          </div>
-          <div>
-            <li>
-              <a
-                href="https://github.com/borjaMarti/educard"
-                className="site-footer__link site-footer__icon"
-              >
-                <FaGithub />
-              </a>
-            </li>
-          </div>
+          <li>
+            <ul>
+              <li>
+                <Link href="/tos" className="site-footer__link">
+                  Condiciones
+                </Link>
+              </li>
+              <li>
+                <Link href="/privacy" className="site-footer__link">
+                  Privacidad
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li className="site-footer__madeby">
+            hecho con ❤️ por{" "}
+            <a
+              href="https://borjamarti.dev/"
+              className="link link--footer"
+              title="Borja Martí Personal Website"
+            >
+              borjaMartí
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/borjaMarti/educard"
+              className="site-footer__link site-footer__icon"
+              title="EduCard's GitHub Repository"
+            >
+              <FaGithub />
+            </a>
+          </li>
         </ul>
       </nav>
     </footer>
